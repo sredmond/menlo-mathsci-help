d4adbba7356271c29a905bf52a66a52a05186fed
Sam Redmond <sam.redmond@menloschool.org>
2013-08-30 00:34:55 -0700

    Revamps the look, nests class selection, and introduces the learn and teach page

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, url_for
 
 DEBUG=True
 app = Flask(__name__)
@@ -7,6 +7,14 @@ app = Flask(__name__)
 @app.route('/')
 def main():
     return render_template('home.html')
+
+@app.route('/learn')
+def learn():
+	return render_template('learn.html')
+
+@app.route('/teach')
+def teach():
+	return render_template('teach.html')
 
 #Run the app
 if __name__ == '__main__':
