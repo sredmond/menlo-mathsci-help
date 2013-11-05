e1e5c0607d50801262f4d949b1efece4c609cedf
Sam Redmond <sam.redmond@menloschool.org>
2013-11-05 15:01:50 -0800

    Adds preliminary email testing

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,14 @@ import os
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.login import LoginManager
-from flask.ext.openid import OpenID
+from flask.ext.mail import Mail
 from config import basedir
 
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
+
+mail = Mail(app)
 
 lm = LoginManager()
 lm.init_app(app)
